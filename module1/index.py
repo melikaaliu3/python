d48eed9e1f89977c6eb93d36dbda6e2547447241
melikaaliu3 <melikaaliu3@gmail.com>
2025-10-23 16:17:42 +0200

    Add files via upload

---
+++ module1/index.py
@@ -0,0 +1,26 @@
+# print('hello world')
+
+temp=98.6
+name='Melika'
+
+print(name)
+
+print(type(name))
+print(type(temp))
+
+print('the best student in class in',name)
+
+x=5
+y=2
+result=x+y
+print(result)
+
+shopping_list=['apples','oranges',3.5,4,5]
+print(shopping_list[2])
+
+shopping_list.append('milk')
+shopping_list.insert(2,'lemons')
+
+shopping_list.remove(3.5)
+shopping_list.pop(1)
+print(shopping_list)
