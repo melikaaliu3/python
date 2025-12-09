0f44b9f6dc640fa96c3e2541dd3851211d231368
Student <melikaaliu3@gmail.com>
2025-12-09 16:10:03 +0100

    module10

---
+++ module10/Encapsulation/decorators.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self,name,age):
+        self.__name=name
+        self.__age=age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self,name):
+        self.__name=name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self,age):
+        self.__age=age
+
+student1=Student("Melika",17)
+
+print("Name:",student1.name)
+student1.name="Redola"
+print("Name:",student1.name)
