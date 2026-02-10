aad56591480cb01e679f31d0e536198e8f9b09e5
Student <melikaaaliu3@gmail.com>
2026-02-10 16:02:51 +0100

    module20

---
+++ module20/main.py
@@ -0,0 +1,46 @@
+#{
+    #"name":"Melika",
+    #"age":17,
+    #"city":"Madrid"
+#}
+
+
+
+{
+    "name":"John Dee",
+    "age":30,
+    "address":{
+        "street":"123 Main St",
+        "city":"Prishtine",
+        "state":"Kosove",
+        "postal code":"10000"
+    },
+    "contacts":[
+        {
+            "type":"email",
+            "value":"johndee@gmail.com"
+        },
+        {
+            "type":"phone",
+            "value":"444-122-4567"
+        }
+    ]
+}
+
+{
+    "fruits":[
+        {
+            "name":"apple",
+            "color":"#FF0000",
+            "details":{
+                "type":'Pome',
+                "season":'Fall'
+            },
+            "nutrients":{
+                'calories':52,
+                'fiber':"2.4g",
+                'vitaminC':"4.6mg"
+            }
+        }
+    ]
+}
